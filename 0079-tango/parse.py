b911b022ca892c4694481f1fdfc6bf61b0d0f2ea
ern0 <ern0@linkbroker.hu>
2021-01-10 22:37:34 +0100

    fixing len < 128 bug

--- 0079-tango/parse.py
+++ 0079-tango/parse.py
@@ -22,48 +22,54 @@ class ParseMidi:
 		self.processTrack(2)
 		self.processTrack(3)
 
-	def processTrack(self, readable):
+		print("note count: " + str(self.noteCounter))
+		print("duration: " + str(self.totalDuration))
 
+
+	def processTrack(self, readableParm):
+
+		self.readable = readableParm
 		self.pitchOffset = 0
 		self.noteCounter = 0
+		self.totalDuration = 0
 		self.stamp = 0
 		self.column = 0
 
-		if readable == 3: print('"', end="")
+		if self.readable == 3: print('"', end="")
 
 		for message in self.track: 
-			self.processMessage(message, readable)
+			self.processMessage(message)
 		
-		if readable == 3: print('"', end="")
+		if self.readable == 3: print('"', end="")
 		print()
 
 
-	def processMessage(self, message, readable):
+	def processMessage(self, message):
 
 		if message.type == "note_on":
 			self.last = message
-		
-		if message.type != "note_off":
-			return
 
-		on = self.last
-		off = message
+		if message.type == "note_off":
+			self.processNote(self.last, message)
+
+
+	def processNote(self, on, off):
 
 		duration = off.time
 		onTime = self.stamp + on.time
 
 		if on.time != 0: 
-			print( self.renderNote(None, on.time, readable), end="")
-			self.newColumn(readable)
-		print( self.renderNote(on.note, duration, readable), end="")
-		self.newColumn(readable)
+			print( self.renderNote(None, on.time), end="")
+			self.newColumn()
+		print( self.renderNote(on.note, duration), end="")
+		self.newColumn()
 
 		self.stamp += on.time + duration
 
 
-	def newColumn(self, readable):
+	def newColumn(self):
 		
-		if readable == 3: return
+		if self.readable == 3: return
 
 		self.column += 1
 		if self.column < 4: 
@@ -73,7 +79,8 @@ class ParseMidi:
 		self.column = 0
 		print()
 
-	def renderNote(self, pitch, duration, readable):
+
+	def renderNote(self, pitch, duration):
 
 		self.noteCounter += 1
 
@@ -88,9 +95,20 @@ class ParseMidi:
 			)[pitch % 12]
 
 		ticks = int( duration / 120 )
+		self.totalDuration += ticks
 
-		if readable == 1:
-			return note + octave + "(" + str(ticks) + ") "
+		if self.readable == 1:
+			return (
+				note 
+				+ octave 
+				+ ":" 
+				+ str(ticks) 
+				+ "(" 
+				+ str(duration) 
+				+ ")"
+			)
+
+		e = note + octave + ":" + str(ticks)
 
 		if pitch is None: 
 			note = 0
@@ -99,22 +117,28 @@ class ParseMidi:
 
 		value = (((ticks-1) << 4) | note) + 34
 
-		if readable == 2:
+		if self.readable == 2:
 			star = "-"
 			if (value > 127): star = "*"
 			return (
-				str(value).rjust(3) + star 
-				+ str(note).rjust(2) + ":" + str(ticks)
-				+ "  "
+				str(value).rjust(3) 
+				+ star 
+				+ str(note).rjust(2) 
+				+ ":" + str(ticks)
+				+ "("
+				+ str(self.totalDuration).rjust(3)
+				+ ")"
 			)
 
 		invalid = False
 		if pitch is not None and note == 0: invalid = True
 		if value < 32: invalid = True
 		if note > 15: invalid = True
-
+		if value == ord("<"): invalid = True
+		if value == ord(">"): invalid = True
+	
 		if invalid: 
-			return "[" + str(note) + "]"
+			return "\" /* " + chr(value) + " " + e + " */ \""
 		else:
 			return chr(value)
 
